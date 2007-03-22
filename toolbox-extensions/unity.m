b4b2ec3e974821d9a268df2b92a6c5c32cd0ef06
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2007-03-22 02:08:36 +0000

    Removing leader parameter from code; updating documentation and various bits of code

--- toolbox-extensions/unity.m
+++ toolbox-extensions/unity.m
@@ -1,25 +1,26 @@
 % --------------------------------------------------------------
 % This function is used to unify the elements in this toolbox so
-% that an mRNA sequence in a text file along with a 12-character
-% leader sequence as a string.  It outputs a displacement plot 
+% that an mRNA sequence in a text file with a 12-character
+% leader sequence as a string can output a displacement plot 
 % and a polar plot using calcmpx.
 %
 % It was created by Hao Lian, Vivek Bhattacharya, and Daniel
 % Vitek to compliment Dr. Lalit Ponnala's toolbox.  It uses a
 % modified version of Dr. Josh Starmer's free2bind program's
-% free_scan.pl.  In the for loop at line 490, just add '&& 0'
-% (without the quotations, of course) to the conditions. This
+% free_scan.pl.  In the for loop at line 490, just add `&& 0'
+% (without quotation marks, of course) to the conditions. This
 % ensures that the output file will only be one line long.
 %
 % USAGE:
-% function [] = unity(file,leader)
-% file = string with the name of the text file, with extension
-% leader = 12-character string of the mRNA leader
+% function [] = unity(file)
+% file = string with the name of the text file, with extension,
+%        that includes the leader
 % --------------------------------------------------------------
 
-function [] = unity(file, leader)
+function [] = unity(file)
 
-if(isempty(which(file))
+file_not_there = isempty(which(file));
+if(file_not_there)
   error(['Cannot find ' file ' in your path']);
 end
 
@@ -31,7 +32,7 @@ S = getseq(file);
 
 % Generate a fasta file for free2bind.
 Fasta = strcat(file, '.fasta');
-write2fasta(Fasta, [S], 'prfb', 60);
+write2fasta(Fasta, S, 'prfb', 60);
 
 % Run free2bind. Make sure to include its directory into -I.
 Include = sprintf('-I"%s"', fileparts(which('FreeAlign.pm')));
@@ -52,7 +53,7 @@ cp = 0; phi_sp=-13*(pi/180); initialx = 0.001; C1 = 0.005; C2 = initialx; Nstop=
 
 %%% demo4 code %%%
 % Run it through the model
-[mag,theta,x] = calcmpx(S(length(leader)+1:end),Signal,phi_sp,Names,TAV,C1,C2,Nstop,spc);
+[mag,theta,x] = calcmpx(S(13:end),Signal,phi_sp,Names,TAV,C1,C2,Nstop,spc);
 
 % Polar plot
 figure(1); polar(theta,mag);
