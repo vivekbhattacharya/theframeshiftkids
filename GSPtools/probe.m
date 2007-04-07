3c0bd9d8c18b0c20239a2067d9a82e392feb4b0e
nemosupport <nemosupport@3eb85987-392c-0410-9b3c-71e71286eceb>
2007-04-07 17:13:05 +0000

    Vivek's lumpy changes

--- GSPtools/probe.m
+++ GSPtools/probe.m
@@ -1,12 +1,27 @@
 function [choice, prob] = probe(x, n1, n2)
 
-k = 1;
-pi = 3.14159;
+k = .333;
+n=2;
 
-p1 = 1 - (cos(x*pi/4)^2)/(k*n2);
-p2 = 1 - (sin(x*pi/4)^2)/(k*n1);
+x = x - 2*floor(abs(x/2));
+disp(x);
 
-if ((p1 + p2) < 0.666); choice = 0; prob = p1;
-elseif (p1 > p2); choice = 0; prob = p1;
-elseif (p2 > p1); choice = 1; prob = p2;
-elseif (p1 == p2); choice = 0; prob = p1; end
+% p1 = 1 - (sin(x*pi/4-k*n1)^(2*n));%*(k*n2);
+% p2 = 1 - (cos(x*pi/4-k*n2)^(2*n));%*(k*n1);
+s1 = (1/n1)^k;
+s2 = (1/n2)^k;
+
+p1 = exp((-(x-0)^2)/(2*s1^2));
+p2 = exp((-(x-2)^2)/(2*s2^2));
+
+p3 = 1 - (p1+p2);
+p = [p1 p2 p3];
+disp(p);
+
+if (max(p)==p(1,3))
+	choice = 2;
+    prob = p3;
+elseif (max(p)==p(1,1)); choice = 0; prob = p1;
+elseif (max(p)==p(1,2)); choice = 1; prob = p2;
+
+end
