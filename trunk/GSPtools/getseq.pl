c26cc6c137b3be9ee71e1e8f2f92ad51f702be3b
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2007-04-08 21:24:29 +0000

    Cooler getseq.pl

--- trunk/GSPtools/getseq.pl
+++ trunk/GSPtools/getseq.pl
@@ -2,19 +2,7 @@
 # Extracts ONLY the character sequence from manually-created file
 
 use strict;
-my $seq = ''; my @seq = ( );
-
-# Input file
-my $infile = $ARGV[0];
-# Read from file
-open(INFILE, "$infile") or die "Cannot open file \"$infile\" for reading!\n\n";
-    @seq = <INFILE>;
-close(INFILE);
-
-# Clean-up
-$seq = join('', @seq);
-# Remove whitespace and line numbers from sequence
-$seq =~ s/[\s0-9]//g;
-
-print $seq;
-exit;
+my $file = shift @ARGV;
+open(F, $file) or die "Cannot open file \"$file\" for reading!\n\n";
+        $_ = join '', <F> and s/[\s0-9]//g and print;
+close(F);
