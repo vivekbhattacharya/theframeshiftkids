7561e47d2215d1308c7309493fe4f082d1e2966e
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2007-04-30 20:31:36 +0000

    By the way, we changed prfb.txt; new nummmmmbies; megaunity

---
+++ trunk/GSPtools/megaunity.m
@@ -0,0 +1,12 @@
+% 0.50667 (150)
+% 0.46825 (126) --shifted prfb
+
+function megaunity(file)
+
+global shoals sands;
+shoals = 0;
+sands = 0;
+while 1
+    unity(file);
+    disp(['ZE CURRENT PROBABBILIBIBIBLBYT ESZ: ' num2str(shoals/sands) ' (' num2str(sands) ')']);
+end
