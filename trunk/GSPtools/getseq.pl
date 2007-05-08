181f12b3eb37109a1bd40d81b8b9c253c9ec9696
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2007-05-08 23:56:30 +0000

    new getseq finally works; perl checks is now earlier

--- trunk/GSPtools/getseq.pl
+++ trunk/GSPtools/getseq.pl
@@ -1,9 +1,8 @@
-
 #!/usr/bin/perl
 # Extracts ONLY the character sequence from manually-created file
 
 use strict;
-my $file = shift @ARGV; 
-open(F, $file) or die "Cannot open file \"$file\" for reading!\n\n"; 
-        $_ = join '', <F> and s/[\s0-9]//g and print; 
-close(F); 
+my $file = shift @ARGV; {
+        open(my $handle, $file) or die "Cannot open file \"$file\" for reading!\n\n";
+        $_ = join '', <$handle> and s/[\s0-9]//g and print;
+}
