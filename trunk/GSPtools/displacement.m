08942207fa9f4f0ac8fa53ddf7227ac1393331a4
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2007-05-28 05:58:05 +0000

    Huge refactor of displacement.m

--- trunk/GSPtools/displacement.m
+++ trunk/GSPtools/displacement.m
@@ -1,14 +1,16 @@
 function [Phase,x,diffx] = displacement(seq,Nstop,spc,Phase,numcodons,Dvec)
-
-phi_sp=-30*(pi/180); initialx = 0.1;
-C1 = 0.005; C2 = initialx;
 % ------------------------------------------------------------
 % CALCULATE DISPLACEMENT 
 % ------------------------------------------------------------    
+
+% Magic numbers
+phi_sp=-30*(pi/180); initialx = 0.1;
+C1 = 0.005; C2 = initialx;
+
 Nloop = []; x = [0 C2]; codon = []; InstPhase = [];
 ants = [];
 
-shift = 0; codons = 0;
+shift = 0;
 for k=2:numcodons-1
     % Choose appropriate codon, depending on the specified spacing, and
     % calculate nloop accordingly
@@ -16,69 +18,57 @@ for k=2:numcodons-1
     try
         codon=seq(initial+1+shift:initial+3+shift);
         other_codon=seq(initial+2+shift:initial+4+shift);
-    catch
-        break;
-    end;
-    codons = [codons codon];
+    catch; break; end;
+
     Nloop(k) = nloopcalcify(codon);
-    other_nloop = nloopcalcify(other_codon);
+    here_loops = real_loops(codon, k);
+    there_loops = real_loops(other_codon, k);
     
-    Nloop_floor = ceil(Nloop(k));
-    other_Nloop_floor = ceil(other_nloop);
-    
-    real_loops = 2^(1/Nloop_floor);
-    real_loops = real_loops / (real_loops - 1);
-    
-    other_real_loops = 2^(1/other_Nloop_floor);
-    other_real_loops = other_real_loops / (other_real_loops - 1);
-    
-    phi_signal(1,k) = Dvec(k,2); x_temp = x(1,k); 
-    
-    phi_dx = ((pi/3)*x_temp)-phi_sp;
-    P_fail_abc = 1;
-    P_fail_bcd = 1;
+    phi_signal(1,k) = Dvec(k,2); x0 = x(1,k);    
+    phi_dx = ((pi/3)*x0)-phi_sp;
+
+    here_fail = 1; there_fail = 1;
     for wt=1:Nstop
-        my_x_temp = x_temp-2*shift;
-        weight_abc = cos(my_x_temp*pi/4)^10;             % Window Function
-        weight_bcd = sin(my_x_temp*pi/4)^10;
-        P_temp_fail_abc = 1- (1/real_loops*weight_abc);
-        P_temp_fail_bcd = 1- (1/other_real_loops*weight_bcd);
-        P_fail_abc = P_fail_abc * P_temp_fail_abc;
-        P_fail_bcd = P_fail_bcd * P_temp_fail_bcd;
-        P_abc = 1-P_fail_abc;
-        P_bcd = 1-P_fail_bcd;
-        P_reloop =1 - P_abc - P_bcd;
-        P = [P_abc P_bcd P_reloop];
-        
+        a = (x0 - 2*shift)*pi/4; % Window function follows     
+        [here_fail, here] = probabilities(here_loops, cos(a)^10, here_fail);
+        [there_fail, there] = probabilities(there_loops, sin(a)^10, there_fail);        
+        reloop = 1 - here - there;
+
         r = rand; % Mersenne Twister
-        if (P_reloop < P_abc) || (P_reloop < P_bcd)
-            if(r < P_abc)
-                break;
-            elseif (r < P_abc + P_bcd)
+        if (reloop < here) || (reloop < there)
+            if(r < here), break;
+            elseif (r < here + there)
                 shift = shift + 1;
                 ants = [ants ' ' codon ',' num2str(k) ';'];
                 break;
             end;
         end;
 
-        phi_dx = ((pi/3)*x_temp)-phi_sp;
+        phi_dx = ((pi/3)*x0) - phi_sp;
         dx = -C1*Dvec(k,1)*sin(phi_signal(1,k) + phi_dx);
-        x_temp = dx + x_temp;
+        x0 = x0 + dx;
     end
     
     InstPhase(k) = (180/pi)*(phi_signal(1,k) + phi_dx);
     if InstPhase(k)<0, InstPhase(k) = InstPhase(k) + 360; end
-    x(1,k+1) = x_temp;
+    x(1,k+1) = x0;
 end
 
 for k=1:length(Phase)
     if Phase(k)<0; Phase(k)=Phase(k)+(2*pi); end
 end
-for k=1:length(x)-1
-    diffx(k)=x(k+1)-x(k);
-end
+for k=1:length(x)-1; diffx(k)=x(k+1)-x(k); end;
 
 %% Counters for megaunity
 global shoals sands;
 sands = sands + 1; disp(ants);
 if(strcmp(ants, ' uga,25;')), shoals = shoals + 1; end;
+
+function [n] = real_loops(codon, index)
+n = ceil(nloopcalcify(codon));
+n = 2^(1/n);
+n = n / (n - 1);
+
+function [acc, p] = probabilities(loops, weight, sofar)
+acc = sofar * (1 - (1/loops*weight));
+p = 1 - acc;
