4b5a869a2f6734b77677ecc18e317a53f9285e7b
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2007-06-05 01:31:40 +0000

    web enabled get_signal.m

--- trunk/GSPtools/get_signal.m
+++ trunk/GSPtools/get_signal.m
@@ -1,8 +1,9 @@
 function [Signal, S] = get_signal(f)
 
 file = which(f);
-if(isempty(file))
+if isempty(file)
     if exist(f) == 2, file = f;
+    elseif strfind(f, 'http://'), file = f;
     else
         error(['Cannot find ``' file '" in your path']);
     end;
@@ -12,7 +13,7 @@ if(isempty('perl.exe')); error(['I cannot find Perl 5.6 or above.' ...
    'Please download it to the VCL C: drive.']); end;
 
 % Load prfb, and generate a fasta file with column width of 60.
-S = getseq(file); Fasta = [file, '.fasta'];
+S = getseq(file); Fasta = tempname;
 write2fasta(Fasta, S, 'prfb', 60);
 
 % Run free2bind. Make sure to include its directory into -I.
