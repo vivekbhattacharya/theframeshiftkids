e453470adca138abe86829355a1119374a20f2b3
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2007-06-05 16:52:10 +0000

    fabio turned on & w/ docs; spring cleaning

--- trunk/GSPtools/fabio.m
+++ trunk/GSPtools/fabio.m
@@ -1,7 +1,12 @@
-% Vivek doesn't want you to use this program
-
 function fabio(freq, names, id)
+% Vivek doesn't want you to use this program.
+%
+% Given a list of frequencies generated from
+% the third link on the search for codon bias,
+% Codons.mat in a text file, and the file name
+% for the .mat file fabio will generate, fabio
+% will do as you wish.
 dire = fileparts(which('Smooth.pm'));
 [S, S] = system(sprintf('perl -I"%s" "%s" "%s" "%s"', dire, which('fabio.pl'), freq, names));
-TAV = str2num(S)
-%save(id, 'TAV');
+TAV = str2num(S);
+save(id, 'TAV');
