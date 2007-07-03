efbc0ba6950ff3c9f274ef3ec75b904f9b858335
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2007-07-03 20:12:06 +0000

    fxsin/xcos/bxsin, reloop, nloops, floatation, upgrading to the latest pearls

--- branches/pyframeshift/ghost.py
+++ branches/pyframeshift/ghost.py
@@ -5,6 +5,27 @@ def steal_out(cmd):
     from subprocess import Popen, PIPE
     return Popen(cmd, stdout=PIPE).stdout
 
-def fxsin(): pass
-def xcos(): pass
-def bxsin(): pass
+from numpy import pi, sin, cos
+period = 4.0
+def fxsin(x):
+    x *= pi/period
+    phase = pi/2 - 2*pi/period
+    
+    if x > (2-period/2) and x < (2+period/2):
+        return sin(x + phase)
+    return 0.0
+
+def xcos(x):
+    x *= pi/period
+    
+    if x > (-period/2.0) and x < (period/2.0):
+        return cos(x)
+    return 0
+
+def bxsin(x):
+    x *= pi/period
+    phase = pi/2.0 + 2.0*pi/period
+    
+    if x > (-2.0-period/2.0) and x < (-2.0+period/2.0):
+        return sin(x + phase)
+    return 0.0
