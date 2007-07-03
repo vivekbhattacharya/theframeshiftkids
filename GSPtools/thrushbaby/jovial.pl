83c6c79f3f4f223a2e0291a9b12f59b6e7884f54
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2007-07-03 23:07:12 +0000

    last rites

--- GSPtools/thrushbaby/jovial.pl
+++ GSPtools/thrushbaby/jovial.pl
@@ -3,36 +3,12 @@ package Jovial;
 use Smooth qw(prot2codon codon2prot getseq);
 
 sub parse {
+    my $garbage = shift;
     return sort { $a->[2] <=> $b->[2] }
         map {
             my ($codon, $loc, $times) = split '_';
             [$times, $codon, $loc];
-        } @_;
-}
-
-# The Kissing Algorithm
-# Imagine you have a bunch of codons with scores
-# sitting single-file. Each codon receives the
-# half the score of the following codon if the
-# two codons are sitting next to each other in
-# a gene sequence. This way, we account for
-# very close frameshifts as the same problem area.
-#
-# Arguments: ([7, 'uga', 25], [3, 'ccc', 49], ...)
-sub distribute_kisses {
-    my $last = $_[0];
-    # ->[0] is the count
-    # ->[2] is the position
-    for (my $i = 1; $i < @_; $i++) {
-        my $now = $_[$i];
-        if ($now->[2] - $last->[2] == 1) {
-            my ($x, $y) = ($now->[0]/2, $last->[0]/2);
-            $last->[0] += $x;
-            $now->[0] += $y;
-        }
-        $last = $now;
-    }
-    @_;
+        } split /\s+/, $garbage;
 }
 
 # Return the codon sequence the comes before
@@ -51,11 +27,14 @@ sub beforehand {
     # Also, ignore the 12-leader (+12) sequence.
     # $i tells us how far we underestimated the location.
     my $breakpoint = $pos*3 + 12 - 3;
-    for (substr $seq, $breakpoint, -1) {
+    for (substr $seq, $breakpoint) {
         my $i = index $_, $data->[1];
-        my $prefix = substr($seq,
-            $breakpoint + $i - $times*3, $times*3);
-        return $prefix;
+        $breakpoint += $i;
+        
+        my $prefix = substr($seq, $breakpoint - $times*3, $times*3);
+        my $before = substr($seq, 0, $breakpoint - $times*3);
+        my $after = substr($seq, $breakpoint);
+        return ($prefix, $before, $after);
     }
 }
 
@@ -69,8 +48,15 @@ sub seq2permutations {
         
 use Data::Dumper;
 if ($0 eq __FILE__) {
-    my @data = (['7','uga',25],['2','aac',26],['3','ccc',49],['5','uaa',57],['2','aug',146],['2','uga',171],['1','ucc',314],['1','cga',315]);
-    my $preseq = beforehand $data[1], 'J:\frameshift\prfb.txt', 4;
-    print Dumper(seq2permutations $preseq);
+    my ($file, $folder, $garbage) = @ARGV;
+    my @data = parse $garbage;
+    my ($critical, $before, $after) = beforehand $data[0], $file, 4;
+    
+    my $i = 1;
+    map {
+        open(my $handle, ">$folder/$i.txt");
+        print $handle ($before . $_ . $after);
+        $i++;
+    } @{seq2permutations $critical};
 }
 1;
