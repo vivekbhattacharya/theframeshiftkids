6e3cf194e00d3f2c216d3aba79be870692e190e2
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2007-07-04 02:55:57 +0000

    runner() in thrushbaby.m, pick communicates with mothership

--- trunk/GSPtools/thrushbaby/thrushbaby.m
+++ trunk/GSPtools/thrushbaby/thrushbaby.m
@@ -1,4 +1,4 @@
-function find_problems(file)
+function thrushbaby(file)
     [Signal, S] = get_signal(file);
     [Mag, Phase, numcodons] = cumm_mag_phase(Signal);
     Dvec = diff_vectors(Mag, Phase, numcodons);
@@ -8,17 +8,50 @@ function find_problems(file)
     beached_whale = 1;
     
     start = 0;
-    while start > -1
+    %while start > -1
         s = struct();
-        for i=1:50
+        for i=1:15
             [x,diffx] = displacement(S(13:end),Phase,numcodons,Dvec,{},{});
             s = helper(s, ants);
             s = helper(s, termites);
         end
-        %stringify(s)
-        pearl('jovial.pl', [num2str(start) ' "' which(file) '" J:\Frameshift\tb1 ' stringify(s)])
-        impunity('J:\Frameshift\tb1', {}, {}, 100);
+        folder = 'J:\Frameshift\tb1';
+        a = pearl('starling.pl', [num2str(start) ' "' which(file) '" "' folder '" ' stringify(s)]);
+        a = eval(a)
+        start = a{1};
+        codon = a{2};
+        runner(folder, 10, codon);
         pause;
+    %end
+end
+
+function [best_name] = runner(folder, limit, codon)
+    d = [dir([folder '/*.txt']); dir([folder '/*.fasta'])];
+    global beached_whale; beached_whale = 1;
+    
+    best_name = ''; best_time = limit*2;
+    global ants termites;
+    for i = 1:length(d)
+        name = d(i).name;
+        
+        % Print the data.
+        [Signal, S] = get_signal([folder '/' name]);
+        [Mag, Phase, n] = cumm_mag_phase(Signal);
+        Dvec = diff_vectors(Mag, Phase, n);
+        time = 0;
+        for i=1:limit
+            displacement(S(13:end), Phase, n, Dvec, {}, {});
+            insects = horzcat(ants, termites);
+            if strmatch(codon, insects)
+                time = time + 1;
+            end
+        end
+        disp([name ': ' num2str(time)]);
+        if time < best_time
+            best_time = time;
+            best_name = name;
+            if time == 0, break; end;
+        end
     end
 end
 
