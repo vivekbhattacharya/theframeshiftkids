1df4a94b39680c89839efc08f171786c3bdfe1d7
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2007-07-05 21:52:49 +0000

    strsmash; permute moves back; docs updated

--- trunk/GSPtools/thrushbaby/starling.pl
+++ trunk/GSPtools/thrushbaby/starling.pl
@@ -13,6 +13,24 @@ sub parse {
         } @_;
 }
 
+# Parses and returns the string segments
+# before and after the needle in the
+# following diagram:
+#       xxxxxxx-->xxxxxxxx<--xxxxx
+#
+# $str := Haystack
+#   $a := Start of the needle
+#   $b := Length of the needle
+# Returns (before, needle, after)
+sub strsmash {
+    my ($str, $a, $b) = @_;
+    return (
+        substr($str, 0, $a-$b),
+        substr($str, $a-$b, $b),
+        substr($str, $a),
+    );
+}
+
 # Return the codon sequence the comes before
 # the given codon. For example, uga's
 # sequence would be ...aggggguaucuu for prfB.
@@ -22,21 +40,19 @@ sub parse {
 # $times: number of codons
 sub beforehand {
     my ($data, $file, $times) = @_;
-    my $pos = $data->[2];
     my $seq = getseq($file);
     
     # Fuzzy search (-3) to account for +1/-1 frameshifts.
     # Also, ignore the 12-leader (+12) sequence.
     # $i tells us how far we underestimated the location.
-    my $breakpoint = $pos*3 + 12 - 3;
-    for (substr $seq, $breakpoint) {
-        my $i = index $_, $data->[1];
-        $breakpoint += $i;
-        
-        my $prefix = substr($seq, $breakpoint - $times*3, $times*3);
-        my $before = substr($seq, 0, $breakpoint - $times*3);
-        my $after = substr($seq, $breakpoint);
-        return ($prefix, $before, $after);
+    #
+    # Also, pull back (modulo) to the nearest *actual*
+    # codon, ignoring all shifting.
+    my $break = $data->[2]*3 + 12 - 3;
+    for (substr $seq, $break) {
+        $break += index $_, $data->[1];
+        $break -= $break % 3;
+        return strsmash($seq, $break, $times*3);
     }
 }
 
@@ -59,10 +75,23 @@ sub pick {
     return $data;
 }
 
+sub permute {
+    use List::Util qw(reduce);
+    no warnings 'once';
+    # Note: we have to store a temporary
+    # $x to access $_ after it's been shadowed.
+    reduce {[
+        map {
+            my $x = $_;
+            map "$_$x", @$a;
+        } @$b
+    ]} @_;
+}
+
 sub seq2permutations {
     my $seq = shift;
     my @proteins = codon2prot Smooth::seq2codons $seq;
-    Smooth::permute map {
+    permute map {
         [split /,/, $Smooth::expression{$_}]
     } @proteins;
 }
@@ -81,21 +110,33 @@ USAGE
         4) and a list of triplets[1]
     
     I will fill the work folder with permutations
-    of the four[2] codons behind the most "common"
+    of the four[2] codons behind[3] the earliest[4]
     frameshifter after the start, including of
     course all the codons surrounding that area.
     
     [1]: In the form of codon_location_occurrence
     [2]: Anything more than four is expensive but
-        possible Contact the authors if you have
+        possible contact the authors if you have
         an extremely fast machine :).
+    [3]: Where behind is behind after correcting
+        any possible shifts that may have occurred
+        so that the replacement codons do not actually
+        change yielded protein.
+    [4]: Technically, jovial considers neighboring
+        codons as one single group and chooses the
+        farthest right neighbor.[5]
+    [5]: A neighbor to a codon is either one away
+        from the codon or one away from another
+        neighbor.
 END
 if ($0 eq __FILE__) {
     if (!@ARGV or $ARGV[0] eq '--help') { print $help; exit }
     
     my ($start, $file, $folder, @rest) = @ARGV;
     my @data = parse $start, @rest;
-    my ($critical, $before, $after) = beforehand pick(@data), $file, 4;
+    # Increase 4 to a higher value if you're feeling
+    # bold and you have a (very) fast computer.
+    my ($before, $critical, $after) = beforehand pick(@data), $file, 4;
     
     my $i = 1;
     map {
