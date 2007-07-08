9e85b12b4b6dd3d2e636527257593884db15e387
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2007-07-08 03:20:26 +0000

    FreeAlign -> Kidnap package

--- trunk/GSPtools/pearls/free_scan.pl
+++ trunk/GSPtools/pearls/free_scan.pl
@@ -25,90 +25,39 @@
 use warnings; use strict;
 
 use Getopt::Std; 
-use FreeAlign;
-
-my $sHelp = <<END_OF_HELP;
-
-Usage: free_scan.pl [options] rna_seq sequence.fasta
-    
-    free_scan takes an RNA binding sequence (like the 3 prime tail on the 16s 
-    rRNA) and sees how well it will bind to the sequence (DNA or RNA, if DNA
-    it is first converted to RNA) at each point in a range specified in 
-    index_data.
-    
-    All of the sequence contained in sequence.fasta will be examined.
-
-    NOTES:
-    The sequence data contained in "sequence.fasta" is assumed to be
-    in 5'->3' orientation.
-
-    "rna_seq" is assumed to be written in 3'->5' orientation.
-
-    example:
-    shell> ./free_scan.pl -p XIA_MATHEWS auuacuagg human_dna.fasta human_gene_indices > delta_G_values.txt
-
-    Commonly used strings for "rna_seq" are 3 prime 16/18 S tails.
-    Here are several listed as reference, written 3prime->5prime:
-
-    E. coli: auuccuccacuag (13 bases)
-    Z. mays: guuacuag (8 bases)
-    Arabidopsis thaliana: guuacuag (8 bases)
-    Xenopus laevis: auuacuag (8 bases)
-    Mus musculus: auuacuag (8 bases)
-    D. melanogaster: auuacuag (8 bases)
-    H. sapiens: auuacuag (8 bases)
-
-Options:
-
--h
-   print this help message.
-
--p FREIER|XIA_MATHEWS
-   Allows you to determine which set of parameters are used to simulate binding
-   between the to strands of nucleotides.  The default value is XIA_MATEHWS.
-   FREIER: RNA binding parameters from 1986
-   XIA_MATHEWS: RNA binding parameters from 1998
-
--t temperature
-   The temperature, in Kelvin, at which the binding is supposed to take place.
-   The default value is 37 + 273.15.
-END_OF_HELP
-
-
+use Kidnap::FreeAlign;
 use Smooth;
+
+sub align_factory {
+    our ($opt_p, $opt_t); getopts('p:t:');
+	$opt_p ||= 'Kidnap::Freier';
+    $opt_t ||= 37 + 273.15;
+    
+    my $o = eval "require $opt_p; $opt_p->new($opt_t)"; die $@ if $@;
+	
+    Kidnap::FreeAlign->new($o);
+}
+
 if ($0 eq __FILE__) {
     Smooth::helpcheck();
-
-    my %opts;
-    getopts('ehdqcL:T:n:fp:t:Ors:g', \%opts);
-     
-    my $align = FreeAlign->new();
-	$opts{'p'} ||= 'FREIER';
-
-	for ($opts{'p'}) {
-		$align->select_parameters($align->FREIER), last if $_ eq 'FREIER';
-		$align->select_parameters($align->XIA_MATHEWS), last if $_ eq 'XIA_MATHEWS';
-		die 'free_scan: Invalid doublet ID parameter (-p)';
-	}
-
-    $align->{Temp} = $opts{'t'} if defined $opts{'t'};
-
+	
+	my $align = align_factory();
 	local @_ = @ARGV;
 	my $rna = shift or die 'free_scan: No RNA binding sequence given';
     my $seq = shift or die 'free_scan: No sequence file given';
     
-	$rna = uc $align->dna2rna($rna);
-    $seq = uc $align->dna2rna(Smooth::getseq $seq);
+	$rna = uc Util::dna2rna($rna);
+    $seq = uc Util::dna2rna(Smooth::getseq $seq);
 	
 	# Analyze sequence data
 	my $rna_length = length $rna;
 	my $seq_length = length $seq;
 
 	for my $i (0 .. $seq_length - $rna_length) {
-		my $seq = substr($seq, $i, $rna_length);
+		my $toys = substr($seq, $i, $rna_length);
 		
 		# Get best binding score
-		$align->force_bind($seq, $rna);
+		$align->bind($toys, $rna);
 		my $free_energy = $align->total_free_energy;
 		
 		# Free energy values greater than zero represent binding
@@ -119,3 +68,101 @@ if ($0 eq __FILE__) {
 	}
 }
 1;
+
+__END__
+
+=head1 NAME
+
+free_scan.pl
+
+=head1 USAGE
+
+  free_scan.pl -p XIA_MATHEWS -t 273.15 auuccuccacuag prfB.fasta
+
+=head1 SYNOPSIS
+
+=over 20
+
+=item B<free_scan.pl>
+
+[B<--help>]
+
+=item B<cornerstone.pl>
+
+[B<-p> I<parameter>] [B<-t> I<temperature (K)>] I<RNA sequence> I<FASTA file>
+
+=back
+
+=head1 DESCRIPTION
+
+free_scan takes an RNA binding sequence (like the 3 prime tail on the 16s 
+rRNA) and calculates the free energy signals that determine how well it
+will bind to the sequence (DNA or RNA) at every point, outputting it.
+Each signal is separated by a newline, allowing Matlab to store the result
+from a C<system> call as a vector.
+
+=head1 OPTIONS AND ARGUMENTS
+
+=over
+
+=item --help
+
+Increases rainfall by an improbably likelihood.
+
+=item -p MODULE
+
+This determines which set of parameters are used to simulate binding
+between the nucleotides strands.  The default module is Freier (1986).
+Alternatively, XiaMathews (1998) is more modern but does not work
+GSPtools. free_scan will search C<@INC> for C<MODULE.pm>.
+
+=item -t NUM
+
+This specifies the temperature, in Kelvin, at which the binding occurs.
+The default value is 37 + 273.15, human body temperature.
+
+=back
+
+=head1 COMMON RNA SEQUENCES
+
+Usually, the RNA sequences are 3' 16/18S tails. Here are several
+written in the 3'->5' orientation.
+
+=over 40
+
+=item I<E. coli>
+
+auuccuccacuag (13 bases)
+
+=item I<Z. mays>
+
+guuacuag (8 bases)
+
+=item I<Arabidopsis thaliana>
+
+guuacuag (8 bases)
+
+=item I<Xenopus laevis>
+
+auuacuag (8 bases)
+
+=item I<Mus musculus>
+
+auuacuag (8 bases)
+
+=item I<D. melanogaster>
+
+auuacuag (8 bases)
+
+=item I<Homo sapiens>
+
+auuacuag (8 bases)
+
+=back
+
+=head1 CAVEATS
+
+The sequence data contained in FASTA file must be in a 5'->3'
+orientation. The RNA sequence must be in a 3'->5' orientation.
+
+=cut
