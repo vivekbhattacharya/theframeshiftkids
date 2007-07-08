7bb8a7dadcecd1270e841c96635ed1546f6eb2f9
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2007-07-08 21:29:42 +0000

    backtrack algorithm; removing scattershot

--- trunk/GSPtools/scattershot.m
+++
@@ -1,8 +0,0 @@
-function scattershot(protein, folder, leader, limit)
-
-for i=1:limit
-    seq = pearl('dysentery.pl', ['"' protein '" "' leader '"']);
-    fid = fopen([folder '/' num2str(i) '.txt'], 'w');
-    fprintf(fid, seq);
-    fclose(fid);
-end
