41b9b7e55b1fc5dabb67cd0d06ff3a46aa719041
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2007-07-16 01:29:11 +0000

    GSPtools 4

--- GSPtools/util/excos.m
+++
@@ -1,8 +0,0 @@
-function [n] = excos(x)
-
-period = 4;
-x = x * pi/period;
-
-if (x > -period/2) && (x < period/2), n = cos(x);
-else n = 0;
-end
--- GSPtools/util/fakeslope.m
+++
@@ -1,4 +0,0 @@
-% Assumptions: x and y are monotone increasing.
-% Linear regression the 5th grade way.
-function [m] = fakeslope(x, y, p)
-m = (y(end) - y(1))/(x(end) - x(1));
