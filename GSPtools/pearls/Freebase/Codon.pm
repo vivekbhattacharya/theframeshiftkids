885aa33461cdeed0117cd7dffb0ebe58001f380a
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2007-07-25 16:02:30 +0000

    Freebase & rebase

---
+++ GSPtools/pearls/Freebase/Codon.pm
@@ -0,0 +1,33 @@
+use strict; use warnings;
+package Codon;
+use constant {
+    POWER => 10,
+    STOP => 1000,
+};
+
+sub new {
+    my $class = shift;
+    
+    my ($loops, $weigh) = @_;
+    my $self = {
+        loops => $loops, weigh => $weigh,
+        fail => 1,
+    };
+    bless $self, $class;
+    return $self;
+}
+
+sub update {
+    my ($self, $window) = @_;
+
+    $window = $self->{weigh}->($window) ** POWER;
+    $self->{fail} *= 1 -  $window / $self->{loops};
+    return 1 - $self->{fail};
+}
+
+sub is_stop {
+    my $self = shift;
+    if ($self->{loops} == STOP) { return -1 }
+    return 0;
+}
+1;
