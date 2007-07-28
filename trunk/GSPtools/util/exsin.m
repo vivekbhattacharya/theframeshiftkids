683895010992448cdca3f493788047db39dcfa05
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2007-07-28 21:48:59 +0000

    Huge mistake corrected

--- trunk/GSPtools/util/exsin.m
+++ trunk/GSPtools/util/exsin.m
@@ -1,16 +1,16 @@
 function [n] = exsin(x, dir)
 
 period = 4;
-x = x * pi/period;
 phase1 = pi/2 - 2*pi/period;
 phase2 = pi/2 + 2*pi/period;
 
+% Draw a graph of sine at x = 0 to see why we're doing this.
 if dir == 117
-    if (x > (2-period/2)) && (x < (2+period/2)), n = sin(x + phase1);
+    if (x > (2-period/2)) && (x < (2+period/2)), n = sin(x * pi/period + phase1);
     else n = 0;
     end
 elseif dir == 771
-    if (x > (-2-period/2)) && (x < (-2+period/2)), n = sin(x + phase2);
+    if (x > (-2-period/2)) && (x < (-2+period/2)), n = sin(x * pi/period + phase2);
     else n = 0;
     end
 end
