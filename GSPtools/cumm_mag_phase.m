10f5e6723224cdc56adf5f243653d386a162a1ab
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2007-08-03 16:51:56 +0000

    cumm_mag_phase speed-up using perma-registers << pyframeshift

--- GSPtools/cumm_mag_phase.m
+++ GSPtools/cumm_mag_phase.m
@@ -6,45 +6,32 @@
 %   x is a row vector that contains the free energy values
 %   Phase is in radians
 
-function [Mag, Phase] = cumm_mag_phase(x)
-    % Signal rounded off to a codon multiple
-    L = length(x);
-    if rem(L,3) ~= 0, x = x(1:L-rem(L,3)); end;
+function [mag, phase] = cumm_mag_phase(signal)
+    % Round signal off to a codon multiple
+    L = length(signal);
     
-    Mag = zeros(1,round(L/3)); Phase = zeros(1,round(L/3));
-    for j=1:L/3
-        [Mag(j), Phase(j)] = helper(x(1:j*3));
-    end
-end
-
-function [A, theta] = helper(x)
-    % This function calculates the magnitude and phase
-    % of a given length of signal using sine-wave
-    % interpolation.
-    % 
-    % INPUTS: 
-    %   x = signal upto a certain number of codons
-    %   The length of the signal must be a codon multiple
-    % 
-    % USAGE:
-    %   [A,theta,Err] = helper(x, avg_choice);
-    %   theta = phase angle in radians
-
-    M = zeros(1,3); % Fill memory registers
-    for j = 1:3:length(x)
-        M = M + x(j:j+2);
-    end    
+    limit = round(L/3);
+    mag = zeros(1, limit); phase = zeros(1, limit);
+    registers = zeros(1,3);
     
-    % The averaging calculation works only for the case when
-    % length(x) is a multiple of 3. Let's assume that is true.
-    % Then calculate magnitude and phase.
-    M = M - mean(M); grass = M(3) + M(2);
-    if M(1) ~= 0
-        theta = atan2(M(1)*sqrt(3), M(1) + 2*M(2)); 
-        A = M(1)/sin(theta);
-    elseif grass ~= 0
-        theta = atan2(grass*sqrt(3), M(3) - M(2));
-        A = -grass/sin(theta);
-    else A = 0; theta = 0;
+    % Calculate the magnitude and phase  of the signal
+    % using sine-wave interpolation.
+    for i=1:limit
+        index = (i-1)*3 + 1;
+        registers = registers + signal(index:index+2);
+        
+        % The averaging calculation works only for the case when
+        % length is a multiple of 3. Let's assume that is true.
+        % Then calculate magnitude and phase.
+        M = registers - mean(registers);
+        grass = M(3) + M(2);
+        if M(1) ~= 0
+            phase(i) = atan2(M(1)*sqrt(3), M(1) + 2*M(2)); 
+            mag(i) = M(1)/sin(phase(i));
+        elseif grass ~= 0
+            phase(i) = atan2(grass*sqrt(3), M(3) - M(2));
+            mag(i) = -grass/sin(phase(i));
+        else mag(i) = 0; phase(i) = 0;
+        end
     end
 end
