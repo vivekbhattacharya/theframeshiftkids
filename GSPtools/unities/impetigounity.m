cd445a08b68fec8d015b842a14476d63e2fdd4bb
nemosupport <nemosupport@3eb85987-392c-0410-9b3c-71e71286eceb>
2007-08-10 14:25:12 +0000

    Impetigounity operates on folders and has varargin

--- GSPtools/unities/impetigounity.m
+++ GSPtools/unities/impetigounity.m
@@ -1,15 +1,78 @@
-function impetigounity(file, times)
+function impetigounity(folder, times, varargin)
 
-displacement = walrus_surprise(file);
-global shoals sands;
+    seth = 1; setv = 1;
 
-waitress = zeros(times, 0);
-for i = 1:times
-    [x, waits] = displacement({}, {});
-    waitress(i, 1:length(waits)) = waits;
+    if length(varargin) == 1
+        if varargin{1} == 0
+            setv = 0;
+        end
+    elseif length(varargin) < 1
+        setv = 0;
+    end;
+    
+    if length(varargin) == 2
+        if varargin{2} == 0
+            setv = 0;
+        end
+    elseif length(varargin) < 2
+        setv = 0;
+    end;
+
+    d = 0; singleton = 0;
+    if isdir(folder)
+        d = [dir([folder '/*.txt']); dir([folder '/*.fasta'])];
+    else
+        d = which(folder);
+        singleton = 1;
+    end
+    
+    subfolder = 'bargraphs';
+    mkdir(fullfile(folder, subfolder));
+
+    function plot_average(file)
+    
+        [folder, file, ext] = fileparts(file);
+
+        displacement = walrus_surprise([file ext]);
+        global shoals sands;
+
+        waitress = zeros(times, 0);
+        for i = 1:times
+            [x, waits] = displacement({}, {});
+            waitress(i, 1:length(waits)) = waits;
+        end
+        
+
+
+        tip = mean(waitress);
+        
+        if seth
+            maxh = varargin{1};
+        else
+            maxh = length(tip);
+        end
+        
+        h = figure(1); set(h, 'Renderer', 'OpenGL');
+            plot(0,0); bar(1:maxh,tip(1:maxh)); title(file);
+            if setv
+                axis([0 maxh+1 0 varargin{2}]);
+            else
+                ylimits = ylim;
+                axis([0 maxh+1 ylim]);
+            end
+            grid; xlabel('Codon Number'); ylabel('Wait Time');
+        saveas(h, fullfile(folder, subfolder, [file '_bar.png']), 'png');
+    
+    end
+    
+    if singleton
+        plot_average(d);
+        return;
+    end
+    
+    for i = 1:length(d)
+        name = d(i).name;
+        plot_average(fullfile(folder, name));
+    end
+       
 end
-
-tip = mean(waitress);
-h = figure(1); set(h, 'Renderer', 'OpenGL');
-    plot(0,0); plot(1:length(tip), tip);
-    grid; xlabel('Codon Number'); ylabel('Wait Time');
