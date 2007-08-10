151808ccf18b2b32051d1935f7d4e06a67b61e8d
nemosupport <nemosupport@3eb85987-392c-0410-9b3c-71e71286eceb>
2007-08-10 16:07:33 +0000

    My mistakes!

--- trunk/GSPtools/pearls/happy_face.pl
+++ trunk/GSPtools/pearls/happy_face.pl
@@ -7,13 +7,11 @@ my ($file) = @ARGV;
 
 open(my $genehandle, $file) or die('Genes.');
 
-my $begin_ecogene = 'http://ecogene.org/ecoSearchProcess.php?name=';
-my $end_ecogene = '&numofresults=20&searchType=gene';
 my $first = 'window.location = ("geneinfo.php?eg_id=';
 
 while (my $gene = <$genehandle>) {
     chomp $gene;
-    my $wholeurl = $begin_ecogene . $gene . $end_ecogene;
+    my $wholeurl = "http://ecogene.org/ecoSearchProcess.php?name=$gene&numofresults=20&searchType=gene";
     my $site = get($wholeurl);
     my $begin = index $site, $first;
     $begin = $begin + length($first);
