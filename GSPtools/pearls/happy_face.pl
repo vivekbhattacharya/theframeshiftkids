ec796d6c2b63fac48b94953fedf6463e223007e1
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2007-08-10 16:24:21 +0000

    HappyFace::thehardpart

--- GSPtools/pearls/happy_face.pl
+++ GSPtools/pearls/happy_face.pl
@@ -1,23 +1,38 @@
 package HappyFace;
-use strict;
-use warnings;
+use strict; use warnings;
 use LWP::Simple;
 
-my ($file) = @ARGV;
-
-open(my $genehandle, $file) or die('Genes.');
-
-my $first = 'window.location = ("geneinfo.php?eg_id=';
-
-while (my $gene = <$genehandle>) {
-    chomp $gene;
+our $tryst = 'window.location = ("geneinfo.php?eg_id=';
+sub get_id {
+    my ($gene) = @_;
     my $wholeurl = "http://ecogene.org/ecoSearchProcess.php?name=$gene&numofresults=20&searchType=gene";
     my $site = get($wholeurl);
-    my $begin = index $site, $first;
-    $begin = $begin + length($first);
+
+    my $begin = index($site, $tryst) + length($tryst);
     my $end = index $site, '"', $begin;
-    my $eg = substr $site, $begin, $end - $begin;
-    print $eg;
+    return substr $site, $begin, $end - $begin;
+}
+
+sub thehardpart {
+    my ($id) = @_;
+    my $url = "http://ecogene.org/dnaSequence.php?eg_id=$id&jointext=&us=12&ds=0";
+    my $site = get($url);
+    
+    my $begin = index $site, ">$id";
+    my $end = index $site, '</PRE>', $begin;
+    return substr $site, $begin, $end - $begin;
+}
+
+
+if (__FILE__ eq $0) {
+    my ($file) = @ARGV;
+    open(my $genehandle, $file) or die('Genes.');
+
+    while (my $gene = <$genehandle>) {
+        chomp $gene;
+        my $id = get_id $gene;
+        print thehardpart $id;
+    }
 }
 
 1;
