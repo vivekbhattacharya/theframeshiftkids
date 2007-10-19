eb051a8f8af4118bbf0b71130fe5c5f93b549b28
nemosupport <nemosupport@3eb85987-392c-0410-9b3c-71e71286eceb>
2007-10-19 19:44:23 +0000

    Sensitivity plots. Abstraction to config.

--- GSPtools/config.m
+++ GSPtools/config.m
@@ -3,7 +3,8 @@ function config
     Config = struct();
     
     % 0: Deviation. 1: Probability.
-    Config.yield = 0;
+    Config.yield = 1;
     
     Config.phi_sp = -30 * pi / 180;
+    Config.init_disp = 0.005;
 end
