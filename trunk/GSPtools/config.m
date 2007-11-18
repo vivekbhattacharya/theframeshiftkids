a0164be9e560f58a64b8acd34f092bfb4df1d729
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2007-11-18 03:34:53 +0000

    Config.dire = 0 Back to -30

--- trunk/GSPtools/config.m
+++ trunk/GSPtools/config.m
@@ -5,9 +5,9 @@ function config
     % 0: Deviation. 1: Probability.
     Config.yield = 1;
     
-    Config.phi_sp = -150 * pi/180;
+    Config.phi_sp = -30 * pi/180;
     Config.init_disp = 0.1;
-    Config.signal_shift = [0 0 0 0];
+    Config.signal_shift = [];
     
     % Normally, don't abort displacement at the first sign of trouble.
     Config.dire = 1;
