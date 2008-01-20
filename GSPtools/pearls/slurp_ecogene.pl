ad1012f2fa1f1dc591288c499845d5c380c5556e
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2008-01-20 01:52:28 +0000

    slurp_ecogene: now smarter about 3'->5' and 5'->3'. Tests added to check new genomes.

--- GSPtools/pearls/slurp_ecogene.pl
+++ GSPtools/pearls/slurp_ecogene.pl
@@ -1,5 +1,4 @@
-use strict;
-use warnings;
+use strict; use warnings;
 use Text::Wrap;
 use Smooth;
 $Text::Wrap::columns = 60;
@@ -9,20 +8,48 @@ Smooth::helpcheck();
 my ($genome_file, $points_file) = @ARGV;
 my $genome = lc Smooth::webslurp $genome_file;
 $genome =~ s/\n//g;
-$genome =~ tr/atgc/uacg/;
+$genome =~ tr/t/u/;
 
-mkdir('genes');
+sub extraction {
+    my ($start, $end) = @_;
+    return substr($genome, $start - 1, $end - $start + 1);
+}
+
+mkdir('genes'); mkdir('confused');
 my %points;
 Smooth::webopen $points_file, sub {
     my ($gene, $start, $end, $desc) = split "\t";
-    open(my $f, ">genes/$gene.txt");
 
-    my $leader = substr($genome, $end, 12);
-    my $seq = substr($genome, $start - 1, $end - $start + 1);
+    my $seq = extraction($start, $end);
+    my $leader = '';
+    my $confused = 0;
+    if ($seq =~ /^(aug|gug)/) {
+        # Oh good, 5' to 3'.
+        $leader = extraction($start - 12, $start - 1);
+    }
+    else {
+        if ($seq =~ /(cau|cac)$/) {
+            # What the hell? 3' to 5'?
+            $leader = extraction($end + 1, $end + 12);
+            $seq = scalar reverse $seq;
+            $seq =~ tr/aucg/uagc/;
 
-    $leader = scalar reverse $leader;
-    $seq = wrap('', '', scalar reverse $seq);
-    print $f ">$desc\n$leader\n$seq";
+            $leader = scalar reverse $leader;
+            $leader =~ tr/aucg/uagc/;
+        }
+        else {
+            $confused = 1;
+        }
+    }
+    $seq = wrap('', '', $seq);
+    if ($confused) {
+        open(my $g, ">confused/$gene.txt");
+        print $g ">$desc (slurp: orientation unknown)\n$seq";
+    }
+    else {
+        open(my $f, ">genes/$gene.txt");
+        print $f ">$desc\n$leader\n$seq";
+    }
 };
 1;
 
@@ -37,15 +64,19 @@ See L<http://ecogene.org/SequenceDownload.php> for the genome. You
 must select the headers "Gene Name", "Left End", "Right End", and
 "Description" and no others.
 
+It automatically detects whether the gene is written 3' to 5' in the
+genome or otherwise, albeity crudely. Genes that do not fit this status
+quo are placed in a "confused" folder.
+
 =head1 SYNOPSIS
 
 =over 20
 
-=item B<cornerstone.pl>
+=item B<slurp_ecogene.pl>
 
 [B<--help>]
 
-=item B<cornerstone.pl>
+=item B<slurp_ecogene.pl>
 
 I<genome file> I<points file>
 
