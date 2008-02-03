af4fdd51df816a158ef3821ee099704259263427
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2008-02-03 00:17:39 +0000

    JSHS branch

--- GSPtools/util/rand_int.m
+++
@@ -1,5 +0,0 @@
-% `count` number of unique random integers in the interval 1:max.
-function [numbers] = rand_int(max, count)
-    [ignore, a] = sort(rand(1, max)); % randperm
-    numbers = a(1:count);
-end
