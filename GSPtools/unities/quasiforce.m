3483f7777f211899ecadc8d7b8bb935ac301043b
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2008-02-19 03:21:37 +0000

    Scattered plot brought to you by democracy.

--- GSPtools/unities/quasiforce.m
+++ GSPtools/unities/quasiforce.m
@@ -1,11 +1,18 @@
 % What the hell? This plots force?
 function quasiforce(file)
+    config();
+    global Travel Names;
+    if isempty(Travel), load Travel2.mat; end
+
     [signal, seq] = get_signal(file);
     [mag, theta] = cumm_mag_phase(signal);
     [dvec, theta] = diff_vectors(mag, theta);
     [x, wts] = displacement(seq(13:end), dvec, []);
     
     theta = theta * 180/pi;
-    figure(1); plot(theta(1:length(wts)), wts, 'LineWidth', 2);
+    figure(1);
+    % displacement uses theta in a way to make an entire codon's worth of
+    % thetas disappear.
+    scatter(theta(1:length(wts)), wts, 49, 'filled');
     grid; xlabel('Angles (degrees)'); ylabel('Weights');
 end
