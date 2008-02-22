5e892a43217e9272a178d1e9dca1cdb7776f0926
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2008-02-22 23:31:52 +0000

    quasiforce updated to clasify.m standards.

--- trunk/GSPtools/unities/quasiforce.m
+++ trunk/GSPtools/unities/quasiforce.m
@@ -1,18 +1,31 @@
-function quasiforce(file)
+function quasiforce(folder)
     config();
-    global Travel Names;
+    global Travel;
     if isempty(Travel), load Travel2.mat; end
 
+    classify(folder, 'sourcefource!', @helper, 'preparation');
+    function helper(path, filename, image)
+        disp(filename);
+        
+        h = graph(filename);
+        [folder, file, ext] = fileparts(image);
+        saveas(h, image, 'png');
+        saveas(h, fullfile(folder, [file '.fig']), 'fig'); 
+    end
+end
+
+function [h] = graph(file)
     [signal, seq] = get_signal(file);
     [mag, theta] = cumm_mag_phase(signal);
     [dvec, theta] = diff_vectors(mag, theta);
     [x, wts] = displacement(seq(13:end), dvec, [25]);
     
     theta = theta * 180/pi;
-    figure(1);
+    h = figure(1);
     % displacement uses theta in a way to make an entire codon's worth of
     % thetas disappear.
     % scatter(theta(1:25), wts(1:25), 49, 'filled');
-    plot(theta(1:25), wts(1:25), 'LineWidth', 2);
-    grid; xlabel('Angles (degrees)'); ylabel('Weights');
+    plot3(theta(1:25), wts(1:25), 1:25, '-.r*', 'LineWidth', 2);
+    campos([-320 -910 73]); grid;
+    xlabel('Angles (degrees)'); ylabel('Weights');
 end
