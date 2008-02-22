54e8af81af54fc56c8244cba7f182cdba9ff783b
nemosupport <nemosupport@3eb85987-392c-0410-9b3c-71e71286eceb>
2008-02-22 21:26:24 +0000

    fakeslope over two points only.

--- trunk/GSPtools/config.m
+++ trunk/GSPtools/config.m
@@ -3,7 +3,7 @@ function config
     Config = struct();
     
     % 0: Deviation. 1: Probability.
-    Config.yield = 0;
+    Config.yield = 1;
     
     Config.init_disp = 0.1;
     Config.phi_sp = -30 * pi/180;
