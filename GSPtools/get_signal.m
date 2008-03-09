e2a21a528c30ae781d6addaadfbd31e07e40226e
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2008-03-09 01:46:12 +0000

    Revamp get_signal.m to use pearl and to vomit better errors.

--- GSPtools/get_signal.m
+++ GSPtools/get_signal.m
@@ -1,36 +1,27 @@
-function [Signal, S] = get_signal(f)
+% I, given a file, call Kidnap for you.
+function [signal, s] = get_signal(f)
 
 file = which(f);
 if isempty(file)
     if exist(f) == 2, file = f;
     elseif strfind(f, 'http://'), file = f;
     else
-        error(sprintf('get_signal cannot find ``%s" in your path. Type which(''%s'') to confirm.', f, f));
-    end;
-end;
+        error(sprintf('get_signal cannot find "%s" in the path. Type which(''%s'') to confirm.', f, f));
+    end
+end
 
-if(isempty('perl')); error(['I cannot find Perl 5.6 or above.' ...
-   'Please download it or add it to your PATH (see website).']); end;
-
-% Load prfb, and generate a fasta file with column width of 60.
-S = getseq(file);
-
-% Run free2bind. Make sure to include its directory into -I.
-Include = fileparts(which('Smooth.pm'));
-   Template = 'perl -I"%s" "%s" auuccuccacuag "%s"';
-   Command = sprintf(Template, Include, which('scan_brightly.pl'), file);
-[status, raw_signal] = system(Command);
+s = pearl('getseq.pl', ['"' file '"']);
+signal = pearl('scan_brightly.pl', sprintf('auuccuccacuag "%s"', file));
 
 % Simulate load() on a string instead of a file.
-Signal = str2num(raw_signal);
-Signal = Signal';
-
 global Config;
-Signal = [Config.signal_shift Signal];
-if(isempty(Signal))
-    ensure = 'I cannot pull signals. Ensure `perl` is outputting the rite stuff.';
-    ensure = [ensure sprintf('\n') 'Also, ensure Perl is of version 5.6 or above.'];
-    ensure = [ensure sprintf('\n') sprintf('\n') 'Perl says: ' raw_signal];
-    ensure = [ensure sprintf('\n') 'Your sequence was: ' S];
+signal = str2num(signal);
+signal = [Config.signal_shift signal'];
+
+if isempty(signal)
+    ensure = sprintf('I cannot pull signals. Ensure `perl -v` shows Perl is of version 5.6 or higher.\n');
+    ensure = [ensure sprintf('Remember to run cleanpath(''path/to/BWFtools''); savepath.\n')];
+    ensure = [ensure sprintf('Perl said for sequence: %s\n', s)];
+    ensure = [ensure sprintf('Perl said for signal: %s\n', signal)];
     error(ensure);
 end
