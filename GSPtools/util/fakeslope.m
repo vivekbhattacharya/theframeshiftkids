9b4ba529378ae0e710d49ff6ffe6b14accd76a5b
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2008-03-14 20:49:13 +0000

    Three codons and about indexing

--- GSPtools/util/fakeslope.m
+++
@@ -1,5 +0,0 @@
-% Linear regression the 5th grade way, assuming the matrix is a filled
-% with two points and I have to calculate the slope of the line
-% connecting them.
-function [m] = fakeslope(ys)
-m = ys(2, :) - ys(1, :);
