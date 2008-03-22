ebeea1dc8444d1af234efcff5f8e109d0775e445
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2008-03-22 19:53:01 +0000

    cumm_energy vectorization overhaul

--- trunk/GSPtools/cumm_energy.m
+++ trunk/GSPtools/cumm_energy.m
@@ -1,5 +1,5 @@
-% This function calculates the cummulative magnitude
-% and phase of the input signal.
+% This function calculates the magnitude and phase of the cummulative
+% energy that arises from the input signal (cf. Kidnap).
 %
 % USAGE:
 %   [mag, phase] = cumm_energy(signal)
@@ -8,33 +8,25 @@
 
 function [mag, phase] = cumm_energy(signal)
     % Round signal off to a codon multiple
-    L = length(signal);
+    limit = floor(length(signal)/3);
+    % Get a matrix of [1 2 3; 4 5 6; 7 8 9; et cetera].
+    indices = reshape(1:limit*3, 3, limit)';
+    % mem stands for "memory" as represented by three registers.
+    mem = signal(indices);
 
-    limit = floor(L/3);
-    mag = zeros(1, limit); phase = zeros(1, limit);
-    registers = zeros(1,3);
-
-    % Calculate the magnitude and phase  of the signal
-    % using sine-wave interpolation.
-    for i=1:limit
-        index = (i-1)*3 + 1;
-        registers = registers + signal(index:index+2);
-
-        % The averaging calculation works only for the case when
-        % length is a multiple of 3. Let's assume that is true.
-        % Then calculate magnitude and phase.
-        %
-        % atan2(...) translates into atan(tan(theta)). Trust us. We
-        % worked it out. (Given r.1 = M * sin(theta), r.2 = M *
-        % sin(theta + 2*pi/3), r.3 = M * sin(theta + 4*pi/3)).
-        %
-        % We center the free energy wave at zero first.
-        M = registers - mean(registers);
-
-        if M(1) ~= 0
-            phase(i) = atan2(M(1)*sqrt(3), M(1) + 2*M(2));
-            mag(i) = M(1)/sin(phase(i));
-        else mag(i) = 0; phase(i) = 0;
-        end
+    for i = 2:limit
+        % Accumulate and center the free energy wave at zero.
+        x = mem(i, :) + mem(i - 1, :);
+        mem(i, :) = x - mean(x);
     end
+
+    % The atan2 expression simplifies into atan(tan(theta)) = theta, given
+    % r.1 = M*sin(theta), r.2 = M*sin(theta + 2*pi/3), r.3 =
+    % M*sin(theta + 4*pi/3). We worked it out.
+    phase = atan2(mem(:, 1)*sqrt(3), mem(:, 1) + 2*mem(:, 2))';
+
+    warning off MATLAB:divideByZero;
+    mag = mem(:, 1)' ./ sin(phase);
+    mag(isnan(mag)) = 0;
+    warning on MATLAB:divideByZero;
 end
