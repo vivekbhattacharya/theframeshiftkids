59656194d5f9f0a95957e6f785e6611827895c30
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2008-03-22 19:55:45 +0000

    Invert memory in cumm_energy

--- trunk/GSPtools/cumm_energy.m
+++ trunk/GSPtools/cumm_energy.m
@@ -10,23 +10,23 @@ function [mag, phase] = cumm_energy(signal)
     % Round signal off to a codon multiple
     limit = floor(length(signal)/3);
     % Get a matrix of [1 2 3; 4 5 6; 7 8 9; et cetera].
-    indices = reshape(1:limit*3, 3, limit)';
+    indices = reshape(1:limit*3, 3, limit);
     % mem stands for "memory" as represented by three registers.
     mem = signal(indices);
 
     for i = 2:limit
         % Accumulate and center the free energy wave at zero.
-        x = mem(i, :) + mem(i - 1, :);
-        mem(i, :) = x - mean(x);
+        x = mem(:, i) + mem(:, i - 1);
+        mem(:, i) = x - mean(x);
     end
 
     % The atan2 expression simplifies into atan(tan(theta)) = theta, given
     % r.1 = M*sin(theta), r.2 = M*sin(theta + 2*pi/3), r.3 =
     % M*sin(theta + 4*pi/3). We worked it out.
-    phase = atan2(mem(:, 1)*sqrt(3), mem(:, 1) + 2*mem(:, 2))';
+    phase = atan2(mem(1, :)*sqrt(3), mem(1, :) + 2*mem(2, :));
 
     warning off MATLAB:divideByZero;
-    mag = mem(:, 1)' ./ sin(phase);
+    mag = mem(1, :) ./ sin(phase);
     mag(isnan(mag)) = 0;
     warning on MATLAB:divideByZero;
 end
