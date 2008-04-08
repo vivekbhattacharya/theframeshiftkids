4a63bb5bfb53e64eca2cd70894eb52ce2fc74927
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2008-04-08 00:30:44 +0000

    Close handle in Smoooooth.pm

--- GSPtools/pearls/getseq.pl
+++ GSPtools/pearls/getseq.pl
@@ -5,13 +5,13 @@ NAME
 
 USAGE
     getseq.pl [path to sequence] [path to fasta file]
-    
+
     getseq.pl retrieves the sequence and prints it.
     In addition, it saves the sequence in a FASTA
     format suitable for free2bind.
 END
 if (!@ARGV or $ARGV[0] eq '--help') { print $help; exit }
-   
+
 # Extracts ONLY the character sequence from manually-created file
 use Smooth;
 my $seq = Smooth::getseq($ARGV[0]);
