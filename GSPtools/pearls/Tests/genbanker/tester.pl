b31ad6908f059131ce012a11e0becbeaf13cf815
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2008-04-23 22:54:21 +0000

    New tester.pl for genbanker

--- GSPtools/pearls/Tests/genbanker/tester.pl
+++ GSPtools/pearls/Tests/genbanker/tester.pl
@@ -1,21 +1,33 @@
 use strict; use warnings;
+use Digest::SHA1;
 
-unless (@_) {
-    print 'I need the absolute path to genbank2ecogene.pl.', $/;
-    exit;
+unless (@ARGV) {
+    die 'I need the absolute path to genbanker.pl.';
 }
 
 my $temp = 'thisisrubbishiapologize';
-my $script = shift;
+my $script = shift @ARGV;
 
-`perl $script goody.txt > $temp`;
-my $output = `diff -u points.txt $temp`;
+`perl "$script" goody.txt genome.txt $temp`;
 
-if ($output) {
-    print 'FAIL', $/, $output, $/;
+my $sha1 = Digest::SHA1->new;
+foreach (qw/a b c/) {
+    open(my $h, "$temp/$_.txt");
+    $sha1->addfile($h);
+    close($h);
+}
+
+use Data::Dumper;
+
+# Be careful! hexdigest destroys the state of $sha1!
+if ($sha1->hexdigest eq 'c205849564a51bfc54b8da7d793f46f411146b49') {
+    print 'ok 1', $/;
 }
 else {
-    print 'ok', $/;
+    print 'FAIL', $/;
 }
 
-unlink($temp);
+use File::Basename qw(dirname);
+use File::Path qw(rmtree);
+use File::Spec;
+rmtree(File::Spec->catdir(dirname(__FILE__), $temp));
