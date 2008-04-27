2aae5eff3b57595ef747c4e414b60f5a1c388311
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2008-04-27 19:28:42 +0000

    Less chdir, more verbosity in Tests/New Genome/test_nostalgia.pl

--- GSPtools/pearls/Tests/New Genome/test_nostalgia.pl
+++ GSPtools/pearls/Tests/New Genome/test_nostalgia.pl
@@ -1,19 +1,21 @@
-use strict;
-use warnings;
-use lib '../..';
+use strict; use warnings;
+
+use File::Basename;
+use lib dirname(__FILE__) . '/../..';
 use Smooth qw(getseq);
 
 my ($old_dir, $new_dir) = @ARGV;
 
-chdir($old_dir);
-my @old = glob("*.txt");
-chdir('..');
+my @old = glob($old_dir . "/*.txt");
 while (1) {
-    my $file_old = $old[rand @old];
+    my $file_old = basename $old[rand @old];
     my $file_new = "$new_dir/$file_old";
     $file_old = "$old_dir/$file_old";
 
-    next if (!-f $file_new);
+    if (!-f $file_new) {
+        print "I can't find $file_old in $new_dir\n";
+        next;
+    }
 
     my $seq_old = Smooth::getseq $file_old;
     my $seq_new = Smooth::getseq $file_new;
