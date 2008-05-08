753550bad6727381458cd105eb155332124b5d38
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2008-05-08 01:11:23 +0000

    Revamp frameshift notation.

--- unities.py
+++ unities.py
@@ -1,18 +1,17 @@
 from matplotlib.pylab import *
 import pull, ghost
 
-def unity(file, fs = []):
+def unity(file, fs = None):
     displacement = walrus_surprise(file, fs)
     places = displacement()
 
-    return
     figure(1)
     plot(range(0, len(places)), places, linewidth=1.0)
     grid(True)
     axis([0, len(places), min(0, min(places)), max(3, max(places))])
     show()
 
-def megaunity(file, fs = []):
+def megaunity(file, fs = None):
     d = walrus_surprise(file, fs)
 
     figure(1); hold(False)
@@ -28,24 +27,13 @@ def walrus_surprise(file, fs):
     from pull import signal, magphase, diff_vectors, displacement
     from numpy import array
 
-    actuary = []
-    if not fs == []:
-        fs = eval(fs)
-        actuary = [0] * (fs[-1] + 1)
-        for i in fs: actuary[i] = 2
-        actuary = array(actuary).cumsum()
-
     signal, seq = signal(file)
     mag, phase, n_codons = magphase(signal)
     diff_vectors = diff_vectors(mag, phase, n_codons)
 
     def walrus():
-        return displacement(seq[12:], diff_vectors, actuary)
+        return displacement(seq[12:], diff_vectors, fs)
     return walrus
 
 if __name__ == '__main__':
-    import sys
-    import cProfile
-
-    cProfile.run('unity(*sys.argv[1:])', 'foo')
-    #megaunity(sys.argv[1])
+    unity('c:/up/prfB.txt', 25)
