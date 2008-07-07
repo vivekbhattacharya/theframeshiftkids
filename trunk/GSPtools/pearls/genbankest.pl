e860a4bc555a72bf8408b29854219d173624c06b
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2008-07-07 17:22:09 +0000

    Cleaning up genbankest.pl, unverified

--- trunk/GSPtools/pearls/genbankest.pl
+++ trunk/GSPtools/pearls/genbankest.pl
@@ -1,30 +1,22 @@
 use LWP::Simple qw(get);
 use File::Path; use 5.010;
+use File::Spec;
 use strict; use warnings;
 
-my $genomelist = $ARGV[0];
+chdir($ARGV[0]);
+my @files = glob('*.txt');
 
-opendir(DIR, $genomelist);
-my @files = grep(/\.txt$/,readdir(DIR));
-closedir(DIR);
-
+my $url = 'http://www.ncbi.nlm.nih.gov/entrez/viewer.fcgi?db=nucleotide&val=%s&sendto=t';
 foreach my $file (@files) {
-    my $accn = substr($file, 0, length($file) - 4);
-    my $url = "http://www.ncbi.nlm.nih.gov/entrez/viewer.fcgi?db=nucleotide&val=%s&sendto=t";
-    my $dir = $genomelist . "/" . $accn;
-    
-    if (!-d $dir) {
-        mkpath($dir);
-    }
-    
-    chdir($genomelist);
-    
-    my $s = get sprintf($url, $accn);
-    open(my $h, ">$accn" . "-genbank.txt") or die "Could not download $accn.";
-    print $h $s;
-    close($h);
-    
-    system('perl C:\up\tools\pearls\genbanker.pl' . " $accn" . "-genbank.txt " . $file . ' ' . $dir);
+    my $accn = substr($file, 0, -n4);
+    my $dir = File::Spec->catdir($genomelist, $accn);
 
-  
+    mkpath $dir if !-d $dir;
+    chdir($genomelist);
+
+    my $s = get sprintf($url, $accn);
+    open(my $h, ">$accn-genbank.txt") or die "Could not download $accn";
+    print $h $s;
+
+    system(sprintf('perl "C:\up\tools\pearls\genbanker.pl %s-genbank.txt %s %s', $accn, $file, $dir));
 }
