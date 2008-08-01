860fb51f60637df2a77757e209309ab96f97d8f7
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2008-08-01 16:40:53 +0000

    Major change: signal to force model.

--- trunk/GSPtools/util/group.m
+++ trunk/GSPtools/util/group.m
@@ -1,4 +1,8 @@
-% Groups vectors together.
+% Groups vectors together. Completely vectorized.
+%
+% USAGE:
+%  >>> group([1 2 2 2 3 4 4 5 6 6 1])
+%  [1 2 3 4 5 6 1; 1 3 1 2 1 2 1]
 function [grouped] = group(vec)
     if length(vec) == 0
         grouped = []; return;
@@ -8,6 +12,8 @@ function [grouped] = group(vec)
         grouped = [vec(1); 1]; return;
     end
 
+    % Obfuscate zeroes.
+    vec(find(vec == 0)) = -42;
     % The starts of new groups will have non-zero values.
     raw_starts = [vec(1) (vec(2:end) - vec(1:end - 1))];
     starts = vec(find(raw_starts));
@@ -15,5 +21,7 @@ function [grouped] = group(vec)
     start_indices = [find(raw_starts) length(vec) + 1];
     % How long is each group?
     lengths = start_indices(2:end) - start_indices(1:end - 1);
+    % Unobfuscate zeroes.
+    starts(find(starts == -42)) = 0;
     grouped = [starts; lengths];
 end
