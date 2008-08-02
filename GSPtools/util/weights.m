14947c286eb7625d12cc02869a068c1c09b13f68
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2008-08-02 16:05:06 +0000

    weights.m: Optimizations.

--- GSPtools/util/weights.m
+++ GSPtools/util/weights.m
@@ -2,10 +2,11 @@
 % x = back. y = current. z = next. The period is four, but we've
 % optimized that away.
 function [w] = weights(x)
-    a = 0; b = 0; c = 0;
-    if (x > -4 && x < 0), a = sin(x * pi/4 + pi); end;
-    if (x > -2) && (x < 2), b = cos(x * pi/4); end;
-    if (x > 0) && (x < 4), c = sin(x * pi/4); end;
-
-    w = [a b c];
+    w = [0 0 0];
+    if (x > -4 && x < 0), w(1) = x * pi/4 + pi; end;
+    % This is cosine.
+    if (x > -2 && x < 2), w(2) = x * pi/4 + pi/2; end;
+    if (x > 0 && x < 4), w(3) = x * pi/4; end;
+    % Conveniently, sin(0) is 0, which avoids a find() for us.
+    w = sin(w);
 end
