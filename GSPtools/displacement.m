e43bc451afa8e48ffa62cb53c8cb382b0a28b6f5
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2008-08-05 17:22:41 +0000

    force, displacement: Smoothing off by 0.5

--- GSPtools/displacement.m
+++ GSPtools/displacement.m
@@ -109,10 +109,10 @@ function [should_break] = loop(piece, k)
         % following three lines into one line.
 
         % slope = next - previous;
-        % intercept = next - slope * n_base_index;
+        % intercept = next - slope * (n_base_index - 0.5);
         % dx = slope * n_base + intercept;
 
-        dx = (next - previous) * (n_base - n_base_index) + next;
+        dx = (next - previous) * (n_base - n_base_index + 0.5) + next;
         displace = displace + Config.c1 * dx;
     end
     store.x(k+1) = displace;
