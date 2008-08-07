e1fabcf80682127860290505884d1f35118afb85
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2008-08-07 15:55:32 +0000

    [major] Removing everything obsoleted by the new force model.

--- trunk/GSPtools/util/diffvec.m
+++
@@ -1,5 +0,0 @@
-function [dy, dx] = diffvec(m1,m2,p1,p2)
-    % m1, p1 is the first vector in polar; m2, p2 the second
-    dx = m2 .* cos(p2) - m1 .* cos(p1);
-    dy = m2 .* sin(p2) - m1 .* sin(p1);
-end
