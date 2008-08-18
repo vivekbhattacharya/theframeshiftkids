00668a4fc18b50aaeb9b4e0ff03bc6c586067ff2
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2008-08-18 15:51:51 +0000

    cleanpath: Better detection of path to old tools.

--- GSPtools/util/cleanpath.m
+++ GSPtools/util/cleanpath.m
@@ -4,17 +4,19 @@ function cleanpath(folder)
     warning off MATLAB:rmpath:DirNotFound;
 
     % What's the old path?
-    old = mfilename('fullpath');
-    % -1 to remove the pathsep.
-    old(strfind(old, 'util')-1:end) = [];
+    old = which('classify.m');
+    if length(old) > 0
+        % -1 to remove the pathsep.
+        old(strfind(old, 'unities')-1:end) = [];
 
-    % Remove the old path.
-    s = path;
-    indices = [0 strfind(s, pathsep)];
-    for i = 1:length(indices)-1
-        idx = indices(i)+1:indices(i+1);
-        t = s(idx);
-        if strfind(t, old); rmpath(t); end;
+        % Remove the old path.
+        s = path;
+        indices = [0 strfind(s, pathsep)];
+        for i = 1:length(indices)-1
+            idx = indices(i)+1:indices(i+1);
+            t = s(idx);
+            if strfind(t, old); rmpath(t); end;
+        end
     end
 
     % Add the new path.
