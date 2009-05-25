66be7acfa3f22efdfc626b095111dd657ad94db6
nemosupport <nemosupport@3eb85987-392c-0410-9b3c-71e71286eceb>
2009-05-25 19:36:22 +0000

    Now shows probabilities as well, along with a red color when you cannot press Enter and a green color when you can.

--- trunk/GSPtools/draw_sine.m
+++ trunk/GSPtools/draw_sine.m
@@ -3,7 +3,7 @@
 % probabilities of frameshifting. Takes the filename of a sequence,
 % like unity!
 function draw_sine(file)
-    global Travel;
+    global Travel store;
     Travel = load_travel();
     [signal, seq] = get_signal(file);
     [mag, phase] = cumm_energy(signal);
@@ -16,19 +16,30 @@ function draw_sine(file)
     old_codon_n = 0;
 
     function helper(x0, probs, codon_n)
-        if codon_n ~= old_codon_n, pause; end;
+        probs
+        num_shift = length(store.anthill) - length(store.termites);
         
         clf;
-        axis([xlim -3 3]);
-        xlabel('Position');
-        ylabel('Magnitude');
-        title(file);
-        subplot(2, 1, 1);
-        draw_one_sine(mag(codon_n), phase(codon_n));
+        subplot(3, 1, 1);
+        draw_one_sine(mag(codon_n), phase(codon_n), file);
 
-        subplot(2, 1, 2);
+        subplot(3, 1, 3);
+        axis([-6 6 0 1]);
+        rectangle('Position', [1 2 3 4]);
+        line([-2 + 2 * num_shift, -2 + 2 * num_shift], [0, probs(1)], 'LineWidth', 2);
+        line([0 + 2 * num_shift, 0 + 2 * num_shift], [0, probs(2)], 'LineWidth', 2);
+        line([2 + 2 * num_shift, 2 + 2 * num_shift], [0, probs(3)], 'LineWidth', 2);
+        
+        subplot(3, 1, 2);
         draw_sequence((codon_n-1)*3 + 2, seq(13:end));
-        rectangle('Position', [x0 - 3, 0.5, 6, 1.0]);
+        fuzzy_bunny1 = xlim; fuzzy_bunny2 = ylim;
+        if codon_n ~= old_codon_n
+            rectangle('Position', [x0 - 3, 0.5, 6, 1.0], 'LineWidth', 2, 'EdgeColor', 'g');
+            pause;
+        else
+            rectangle('Position', [x0 - 3, 0.5, 6, 1.0], 'LineWidth', 2, 'EdgeColor', 'r');
+        end
+                
         pause(0.01);
         
         old_codon_n = codon_n;
@@ -38,8 +49,12 @@ function draw_sine(file)
 end
 
 % Draws one sine wave like it says.
-function draw_one_sine(mag, phase)
+function draw_one_sine(mag, phase, file)
     x = -6:0.1:6;
-    y = mag*sin((2/3)*pi*(x - phase));
+    y = mag*sin((2/6)*pi*(x - phase));
     plot(x, y);
+        axis([xlim -3 3]);
+        xlabel('Position');
+        ylabel('Magnitude');
+        title(file);
 end
