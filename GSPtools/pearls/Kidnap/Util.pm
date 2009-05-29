a9888101b02b29d8703d8b945662e943dcb962df
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2009-05-29 15:04:48 +0000

    Cleanup: Kidnap::Util.

--- GSPtools/pearls/Kidnap/Util.pm
+++ GSPtools/pearls/Kidnap/Util.pm
@@ -1,25 +1,26 @@
-package Util;
+package Kidnap::Util;
 use strict; use warnings;
 
 ## Returns true if two characters form a valid helix pair,
 ## i.e. a standard Watson-Crick pair or a G-U mismatch.
 sub valid_pair {
     my ($a, $b) = @_;
-	my $ab = $a . $b;
-	scalar grep /$ab/, qw/GC CG AU UA GU UG/;
+    my $ab = $a . $b;
+    scalar grep /$ab/, qw/GC CG AU UA GU UG/;
 }
 
 
 ## Returns true if two characters form a valid W-C pair
 sub wc_pair {
     my ($a, $b) = @_;
-	my $ab = $a . $b;
-	scalar grep /$ab/, qw/GC CG AU UA/;
+    my $ab = $a . $b;
+    scalar grep /$ab/, qw/GC CG AU UA/;
 }
 
 sub dna2rna {
     my $seq = shift;
     $seq =~ tr/tT/uU/;
-	return $seq;
+    return $seq;
 }
+
 1;
