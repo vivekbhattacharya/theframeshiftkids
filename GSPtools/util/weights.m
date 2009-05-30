b0ee47d30de809640111bb980d1f9d3a953c2c1f
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2009-05-30 19:48:50 +0000

    Cleanup: displacement and weights.

--- GSPtools/util/weights.m
+++ GSPtools/util/weights.m
@@ -3,10 +3,19 @@
 % optimized that away.
 function [w] = weights(x)
     w = [0 0 0];
-    if (x > -4 && x < 0), w(1) = x * pi/4 + pi; end;
+    if (x > -4 && x < 0)
+        w(1) = x * pi/4 + pi;
+    end
+
     % This is cosine.
-    if (x > -2 && x < 2), w(2) = x * pi/4 + pi/2; end;
-    if (x > 0 && x < 4), w(3) = x * pi/4; end;
+    if (x > -2 && x < 2)
+        w(2) = x * pi/4 + pi/2;
+    end
+
+    if (x > 0 && x < 4)
+        w(3) = x * pi/4;
+    end
+
     % Conveniently, sin(0) is 0, which avoids a find() for us.
     w = sin(w);
 end
