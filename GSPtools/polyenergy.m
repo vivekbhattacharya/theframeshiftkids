093f7211ec8797cb2b05ceb33d636b9e2efdbf5c
nemosupport <nemosupport@3eb85987-392c-0410-9b3c-71e71286eceb>
2009-05-31 22:01:03 +0000

    Got rid of Config.codon_spacing since it was not doing anything; added comments to polyenergy.

--- GSPtools/polyenergy.m
+++ GSPtools/polyenergy.m
@@ -1,9 +1,17 @@
+% A function that takes a codon number and returns the energy polynomial
+% and function to calculate the force from the codon number and the
+% position
 function [energy, polyforce] = polyenergy(codon)
-    global store;
+    global store Config;
+    
+    % Pad the signal with zeroes in case we use larger powers.
     new_signal = [zeros(1, 6) store.signal zeros(1, 6)];
-    center = 3*codon + 5 + store.shift - 2;
+    center = 3*codon + 5 + store.shift - Config.signal_shift;
 
-    power = 4;
+    % Defines power, bounds for the codons, and the limits of
+    % regression.  Takes into account that a distance of one
+    % nucleotide is actually 2 in terms of displacement.
+    power = 4
     lower = floor(-power/2);
     upper = floor(power/2);
     x = lower:upper;
@@ -12,6 +20,10 @@ function [energy, polyforce] = polyenergy(codon)
     energy = polyfit(x, y, power);
 
     function [dx, force] = helper(codon, x)
+        % F = -dU/dx; need to drop the last element of the force
+        % vector after differentiation because Matlab starts the
+        % vector with the coefficient of the highest power of
+        % the polynomial.
         force = -energy .* (power:-1:0);
         force = force(1:power);
         dx    = polyval(force, x - 2*store.shift);
