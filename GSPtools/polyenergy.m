3ed9cb242fbc81ee42aa0734d0101c4ebc571748
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2009-06-01 01:55:31 +0000

    Cleanup: polyenergy.m.

--- GSPtools/polyenergy.m
+++ GSPtools/polyenergy.m
@@ -3,7 +3,7 @@
 % position
 function [energy, polyforce] = polyenergy(codon)
     global store;
-    
+
     % Pad the signal with zeroes in case we use larger powers.
     new_signal = [zeros(1, 6) store.signal zeros(1, 6)];
     center = 3*codon + 5 + store.shift - 2;
@@ -11,11 +11,10 @@ function [energy, polyforce] = polyenergy(codon)
     % Defines power, bounds for the codons, and the limits of
     % regression.  Takes into account that a distance of one
     % nucleotide is actually 2 in terms of displacement.
-    power = 4
+    power = 4;
     lower = floor(-power/2);
     upper = floor(power/2);
-    x = lower:upper;
-    x = 2*x;
+    x = 2*(lower:upper);
     y = new_signal(center+lower:center+upper);
     energy = polyfit(x, y, power);
 
