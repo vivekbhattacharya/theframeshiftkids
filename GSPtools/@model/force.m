e47bfbbfa85c114a386a2ba54e167e3a7439c54b
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2009-06-04 22:41:18 +0000

    Feature: correct for species angle in Bitzer's @model.

--- GSPtools/@model/force.m
+++ GSPtools/@model/force.m
@@ -2,11 +2,13 @@
 % the force at the given position, given the codon number and energy
 % vector.
 function dx = force(model, energy, codon, x)
+    global Config;
     % F = -dU/dx. We need to drop the last element of the force vector
     % after differentiation because Matlab starts the vector with the
     % coefficient of the highest power of the polynomial.
     power = model.power;
     force = -energy .* (power:-1:0);
     force = force(1:power);
+    x     = x - 3/pi*Config.phi_sp;
     dx    = polyval(force, x - 2*model.shift);
 end
