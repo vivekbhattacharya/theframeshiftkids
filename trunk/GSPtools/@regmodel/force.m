4f66931eb5a9874606f041f976fb48b61a213af3
shadytrees <shadytrees@3eb85987-392c-0410-9b3c-71e71286eceb>
2009-06-06 04:30:26 +0000

    Documentation: @regmodel/force.

--- trunk/GSPtools/@regmodel/force.m
+++ trunk/GSPtools/@regmodel/force.m
@@ -1,6 +1,10 @@
 % A method for the @regmodel class, part of the loop() protocol.
-% Returns the force at the given position, given the codon number and
-% energy vector according to Dr. Ponnala's register model.
+% Returns the force at the given position and energy vector according
+% to Dr. Ponnala's register model.
+%
+% As a kludge, the "energy vector" is actually the codon position
+% because we compute the force at all possible points during object
+% construction.
 function dx = force(self, shift, codon, x)
     global Config;
 
